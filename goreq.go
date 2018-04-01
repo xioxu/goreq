fb577bbf28f2b7e276401320824e53f40a46a782
tonyxu123 <mailme.xu@gmail.com>
2018-04-01 22:27:08 +0800

    supports both http/https proxy

--- goreq.go
+++ goreq.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 	"io/ioutil"
 	"net/url"
+	"strings"
+	"bytes"
+	"io"
+	"encoding/json"
+	"net/http/cookiejar"
+	"compress/gzip"
 )
 
 var defaultTransport http.RoundTripper = &http.Transport{MaxIdleConns: 10, IdleConnTimeout: 30 * time.Second,}
@@ -12,7 +18,7 @@ var defaultClient = &http.Client{Transport: defaultTransport}
 var defaultOptions = DefaultOptions()
 
 type GoReq struct {
-	options   *ReqOptions
+	Options   *ReqOptions
 	client    *http.Client
 	transport *http.Transport
 }
@@ -31,24 +37,82 @@ type ReqOptions struct {
 	FollowRedirect bool
 
 	// if not nil, remember cookies for future use (or define your custom cookie jar; see examples section)
-	Jar *http.Cookie
+	Jar *cookiejar.Jar
 
 	//an HTTP proxy url to be used
 	Proxy string
+
+	//object containing querystring values to be appended to the uri
+	QueryString url.Values
+
+	bodyContent httpReqBody
+}
+
+type httpReqBody interface {
+	build() (contentType string, data io.Reader)
+}
+
+type formContent struct {
+	content url.Values
+}
+
+func (form *formContent) build() (contentType string, data io.Reader) {
+	return "application/x-www-form-urlencoded", strings.NewReader(form.content.Encode())
+}
+
+type jsonContent struct {
+	content []byte
+}
+
+func (jsonString *jsonContent) build() (contentType string, data io.Reader) {
+	return "application/json", bytes.NewReader(jsonString.content)
+}
+
+type jsonObjContent struct {
+	content interface{}
+}
+
+func guard(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (jsonObj *jsonObjContent) build() (contentType string, data io.Reader) {
+	bytesContent, err := json.Marshal(jsonObj.content)
+	guard(err)
+	return "application/json", bytes.NewReader(bytesContent)
+}
+
+func (options *ReqOptions) buidUrl() string {
+	url := options.Url
+
+	qs := options.QueryString.Encode()
+
+	if qs != "" {
+		if strings.Contains(qs, "?") {
+			url += "&" + qs;
+		} else {
+			url += "?" + qs;
+		}
+	}
+
+	return url
 }
 
 func DefaultOptions() *ReqOptions {
 	options := ReqOptions{
 		Method:         "Get",
 		FollowRedirect: false,
-		Headers:make(map[string]string),
+		Headers:        make(map[string]string),
+		QueryString:    make(url.Values),
 	}
 
 	return &options
 }
 
-func Options(opts *ReqOptions)  *ReqOptions{
-	return mergeOptions(opts,defaultOptions)
+func Options(opts *ReqOptions) *ReqOptions {
+	return mergeOptions(opts, defaultOptions)
 }
 
 func request(url string, callback func(error, *http.Response, []byte)) {
@@ -91,7 +155,7 @@ func mergeOptions(copyTo *ReqOptions, copyFrom *ReqOptions) *ReqOptions {
 		copyTo.Url = copyFrom.Url
 	}
 
-	if copyTo.Jar != nil {
+	if copyTo.Jar == nil {
 		copyTo.Jar = copyFrom.Jar
 	}
 
@@ -104,59 +168,100 @@ func mergeOptions(copyTo *ReqOptions, copyFrom *ReqOptions) *ReqOptions {
 
 func Req(options *ReqOptions) *GoReq {
 	goReq := GoReq{}
-	goReq.transport = &http.Transport{}
+	goReq.transport = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+	}
 	goReq.client = &http.Client{
 		Transport: goReq.transport,
 	}
-	goReq.transport.Proxy = http.ProxyFromEnvironment
-	goReq.options = mergeOptions(options, defaultOptions)
+
+	goReq.Options = mergeOptions(options, defaultOptions)
 	return &goReq
 }
 
 func (req *GoReq) Post(url string) *GoReq {
-	req.options.Url = url;
-	req.options.Method = "POST"
-	return  req
+	req.Options.Url = url;
+	req.Options.Method = "POST"
+	return req
 }
 
 func (req *GoReq) Get(url string) *GoReq {
-	req.options.Url = url;
-	req.options.Method = "Get"
-	return  req
+	req.Options.Url = url;
+	req.Options.Method = "Get"
+	return req
 }
 
-func (req *GoReq) Do() (  []byte,*http.Response,error) {
-	if req.options.Proxy != "" {
-		parsedProxyUrl, err := url.Parse(req.options.Proxy)
+func (req *GoReq) Form(formData url.Values) {
+	req.Options.bodyContent = &formContent{
+		content: formData,
+	}
+}
+
+func (req *GoReq) JsonString(jsonStr []byte) {
+	req.Options.bodyContent = &jsonContent{
+		content: jsonStr,
+	}
+}
+
+func (req *GoReq) JsonObject(jsonObj interface{}) {
+	req.Options.bodyContent = &jsonObjContent{
+		content: jsonObj,
+	}
+}
+
+func (req *GoReq) Do() ([]byte, *http.Response, error) {
+	if req.Options.Proxy != "" {
+		parsedProxyUrl, err := url.Parse(req.Options.Proxy)
 
 		if err != nil {
-			return nil, nil,err
+			return nil, nil, err
 		} else {
 			req.transport.Proxy = http.ProxyURL(parsedProxyUrl)
 		}
 	}
 
-	httpReq, err := http.NewRequest(req.options.Method, req.options.Url, nil)
-	if err != nil {
-		return nil, nil,err
+	if req.Options.Jar != nil {
+		req.client.Jar = req.Options.Jar
 	}
 
-	if req.options.Headers != nil {
-		for k, v := range req.options.Headers {
+	var submitBody io.Reader
+	var contentType string
+	if req.Options.bodyContent != nil {
+		contentType, submitBody = req.Options.bodyContent.build()
+		req.Options.Headers["Content-Type"] = contentType
+	}
+
+	httpReq, err := http.NewRequest(strings.ToUpper(req.Options.Method), req.Options.buidUrl(), submitBody)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if req.Options.Headers != nil {
+		for k, v := range req.Options.Headers {
 			httpReq.Header.Add(k, v)
 		}
 	}
 
 	resp, err := req.client.Do(httpReq)
 	if err != nil {
-		return nil, nil,err
+		return nil, nil, err
 	}
 
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, nil,err
+
+	var reader io.ReadCloser
+	switch resp.Header.Get("Content-Encoding") {
+	case "gzip":
+		reader, err = gzip.NewReader(resp.Body)
+		defer reader.Close()
+	default:
+		reader = resp.Body
 	}
 
-	return  body,resp,nil
+	body, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return body, resp, nil
 }
