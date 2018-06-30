53c07232ee94f4066caa4f73187550e34f8aebab
TonyXu <mailme.xu@gmail.com>
2018-06-30 11:50:45 +0800

    Add code sample

--- example_test.go
+++ example_test.go
@@ -1,10 +1,35 @@
 package goreq
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func ExampleGoReq_Req() {
+	req := Req(&ReqOptions{Proxy:&NullableString{Value:"http://localhost:8888"}})
+
+	// req1 still keeps the options of req
+	req1 := req.Req(nil)
+	req1.Get("http://www.baidu.com").Do()
+}
+
+func ExampleReq() {
 	req := Req(nil)
 	body,_,_ := req.Get("https://www.baidu.com").Do()
 	fmt.Print(string(body))
+}
+
+func ExampleGoReq_PipeReq() {
+	if err := http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		req := Req(&ReqOptions{
+			Method: r.Method,
+			Url:    "https://www.baidu.com" + r.RequestURI,
+		})
+
+		req.PipeFromReq(r).PipeToResponse(w)
+	})); err != nil {
+		panic(err)
+	}
 }
 
